169fcf027ba8fc41d069314185d8f46955062cf6
Marcio Ronaldo Alves <66127342+Marcio-R@users.noreply.github.com>
2023-03-31 16:18:36 -0300

    Criado tela e ação delete.

--- Projeto_MVC/Controllers/CursoController.cs
+++ Projeto_MVC/Controllers/CursoController.cs
@@ -49,5 +49,62 @@ namespace Projeto_MVC.Controllers
                 throw new Exception(ex.Message);
             }
         }
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso.SingleOrDefaultAsync(x => x.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            return View(curso);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Curso curso, int id)
+        {
+            if (id != curso.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(curso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(curso);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var curso = await _context.Curso.FirstOrDefaultAsync(x => x.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            return View(curso);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirma(int id)
+        {
+            var curso = await _context.Curso.FirstOrDefaultAsync(x => x.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(curso);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
